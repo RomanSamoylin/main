00600aa6b0e3350208c6cc4f9c6a1a8bf2ae95fd
Roman Samoylin <roman.samoylin@gmail.com>
2022-08-24 21:24:51 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("Число " + a + " больше " + b);
+}
+else
+{
+   if (a == b)
+   {
+        Console.WriteLine("Число " + b + " равно " + a);
+   }
+   else
+   {
+        Console.WriteLine("Число " + b + " больше " + a);
+   }
+}
