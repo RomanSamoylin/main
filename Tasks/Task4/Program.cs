225d7857eb2e63bafd2a03f85ef261295b0dcebb
Roman Samoylin <roman.samoylin@gmail.com>
2022-08-24 22:34:13 +0300

    Task4

---
+++ Tasks/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+    if (c > max)
+    {
+        Console.WriteLine("Число " + c + " самое большое ");
+    }
+    else
+    {
+        Console.WriteLine("Число " + b + " самое большое ");
+    }
+}
+else
+{
+    if (c > a)
+    {
+        Console.WriteLine("Число " + c + " самое большое ");
+    }
+    else
+    {
+        Console.WriteLine("Число " + a + " самое большое ");
+    }
+}
