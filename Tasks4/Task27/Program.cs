0fd898e59d9769a5d1eb8c803c382546d58d343f
Roman Samoylin <roman.samoylin@gmail.com>
2022-08-29 21:08:03 +0300

    Task27

---
+++ Tasks4/Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumMeMath(int a, string msg)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    Console.WriteLine($"Cумма цифр равна {sum}. ({msg})");
+}
+
+void SumMeString(string input, string msg)
+{
+    int res = 0;
+    string[] a = new string[input.Length];
+    for (int i = 0; i < input.Length; i++)
+    {
+        a[i] = input[i].ToString();
+        res += Convert.ToInt32(a[i]); 
+    }
+    Console.WriteLine($"Cумма цифр равна {res}. ({msg})");
+}
+
+int number = ReadInt("Введите число: ");
+string numero = Convert.ToString(number);
+
+SumMeMath(number, "Математическим методом");
+SumMeString(numero, "Строчным методом");
