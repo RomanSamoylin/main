f8a5fb11f8a3c3fd5c4d53868d5bd7b58d2a32a6
Roman Samoylin <roman.samoylin@gmail.com>
2022-08-29 21:10:46 +0300

    Task29

---
+++ Tasks4/Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая задаёт массив из N элементов и выводит их на экран
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray (int[] a)
+{
+    for (int i=0; i<a.Length; i++) Console.Write(a[i]+" ");
+}
+
+int[] CreateArray (int size)
+{
+    int[] a = new int[size];
+    for (int i=0; i<a.Length; i++) a[i] = new Random().Next(0, 100);
+    return a;
+}
+
+PrintArray(CreateArray(ReadInt("Введите размер желаемого массива: ")));
