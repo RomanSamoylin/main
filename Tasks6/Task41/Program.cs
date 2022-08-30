e9e837d74abe050c3b4e2a2b041fc865968be32b
Roman Samoylin <roman.samoylin@gmail.com>
2022-08-30 20:53:50 +0300

    Task41

--- Tasks6/Task41/Program.cs
+++ Tasks6/Task41/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+int CountCommas(string input)
+{
+    int counter = 0;
+    for (int i = 0; i < input.Length; i++)
+        if (input[i] == ',')
+            counter++;
+    return counter;
+}
+
+int CountMinuses(string input)
+{
+    int counter = 0;
+    for (int i = 0; i < input.Length; i++)
+        if (input[i] == '-')
+            counter++;
+    return counter;
+}
+
+int CountZeros(string input)
+{
+    int counter = 0;
+    for (int i = 0; i < input.Length; i++)
+        if (input[i] == '0' && input[i-1] == ',' && input[i+1] == ',')
+            counter++;
+    return counter;
+}
+
+/*--------MAIN-----------*/
+Console.Write("Введите цифры через запятую: ");
+string userInput = Console.ReadLine() + ",";
+userInput = userInput.Replace(" ", "");
+int numbersCount= CountCommas(userInput);
+if (userInput.Length==numbersCount)
+    {
+        Console.Write ("Цифр не обнаружено...");
+        return;
+    }
+int zeros = CountZeros(userInput);
+int minuses = CountMinuses(userInput);
+Console.WriteLine (numbersCount-zeros-minuses);
