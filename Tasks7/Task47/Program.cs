0bbdc4196ce25eba82abd44ca90aa5e0d12c3649
Roman Samoylin <roman.samoylin@gmail.com>
2022-09-01 20:28:02 +0300

    Task47

---
+++ Tasks7/Task47/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+double[,] array = new double[rows, columns];
+FillArray2D(array);
+PrintArray2D(array);
+
+
+/*------------METHODS------------------*/
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10)+new Random().Next(0, 10)*0.1;
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
