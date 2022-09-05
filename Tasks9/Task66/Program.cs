b8bae01bc024c9a942f86b4ecf317d3b01fae731
Roman Samoylin <roman.samoylin@gmail.com>
2022-09-05 20:36:57 +0300

    Task66

---
+++ Tasks9/Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int RangeSum (int n, int m)
+{
+    
+    if (m == n)
+        return m;
+    else 
+    {
+        int sum = m;
+        sum+=RangeSum(n, m -1);
+        return sum;
+    }
+}
+
+int number1 = ReadInt("Введите число: ");
+int number2 = ReadInt("Введите 2e число: ");
+Console.Write(RangeSum(number1, number2));
