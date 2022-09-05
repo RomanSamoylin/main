5bd482de3c2e357147f54626c397d81e01260ddd
Roman Samoylin <roman.samoylin@gmail.com>
2022-09-05 20:35:19 +0300

    Task64

---
+++ Tasks9/Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NaturalReverseRange(int n)
+{
+    if (n <= 2) 
+        return 1;
+    else
+    {
+
+        n--;
+        Console.Write ($"{n} ");
+        n=NaturalReverseRange(n);
+    }
+    return n;
+}
+
+
+int number = ReadInt("Введите число: ");
+Console.Write ($"{NaturalReverseRange(number+1)} ");
